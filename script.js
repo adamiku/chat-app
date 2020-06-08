541f787e54521850e035e3fca7fc5a95270ddccb
Adam Mikulas <admi01@betssongroup.com>
2020-06-08 22:30:05 +0200

    feat(script): add listening and emiting functionality

---
+++ script.js
@@ -0,0 +1,39 @@
+const SERVER_IP_ADDRESS = 'http://35.157.80.184:8080/';
+
+const socket = io(SERVER_IP_ADDRESS);
+
+const messageFormContainer = document.getElementById('message-form-container');
+const messageContainer = document.getElementById('message-list-container');
+const messageUser = document.getElementById('message-user');
+const messageText = document.getElementById('message-text');
+
+let userName = '';
+
+socket.on('message', ({ user, message }) => {
+    if (user === userName) return;
+    appendMessage(`${user}: ${message}`);
+})
+
+messageFormContainer.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const message = messageText.value;
+    userName = messageUser.value;
+
+    if (message === '') return;
+
+    appendMessage(`${message}`, true);
+
+    socket.emit('message', { message, user: userName});
+    messageText.value = '';
+})
+
+function appendMessage(message, ownMessage = false) {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    
+    if (ownMessage) messageElement.classList.add("ownMessage");
+    
+    messageContainer.append(messageElement);
+    messageElement.scrollIntoView();
+}
